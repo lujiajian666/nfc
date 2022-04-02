4ff24bb281a798d9996db59e9b91414c1c5d7119
lujiajian <18928728949@163.com>
2022-04-02 09:58:54 +0800

    init: 初始化

---
+++ src/page/Index/component/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.less';
+
+function Header() {
+  return (
+    <div className="header">
+      hader
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/page/Index/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+import Header from './component/Header';
+
+function App() {
+  console.debug();
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
